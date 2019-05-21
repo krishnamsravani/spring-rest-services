86f507bdabcdfcfb0b8fd9b057c8710ad2dcabe9
krishnam sravani <sravanikrishnam369@gmail.com>
2019-05-21 14:19:01 +0530

    initial commit

---
+++ src/main/java/com/stackroute/userservice/domain/User.java
@@ -0,0 +1,72 @@
+package com.stackroute.userservice.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User
+{
+    public User() {
+    }
+
+    @Id
+    int id;
+    String firstname;
+    String lastname;
+    int age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+
+    public User(int id, String firstname, String lastname, int age) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+
+}
